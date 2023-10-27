da1a63a4bb8461ae80ba9503583c59a4c430e2c4
Bruno Santos <79641320+Lemonzito@users.noreply.github.com>
2023-10-27 16:12:04 -0300

    Envio completo de todo o código.
    Este programa já havia sido subido, mas para organizar melhor o perfil.

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Calculadora";
+            Console.WriteLine("***************************************Calculadora***************************************");
+
+            double num1 = 0, num2 = 0, prod, num;
+            string operador, valorEscolhido;
+            bool isNumeroInteiro;
+
+            Console.WriteLine("Digite o primeiro valor:");
+            valorEscolhido= Console.ReadLine();
+
+            isNumeroInteiro = double.TryParse(valorEscolhido, out num);
+            if (isNumeroInteiro)
+            {
+                num1 = Math.Round(double.Parse(valorEscolhido), 4);
+            }
+            else
+            {
+                Console.WriteLine("Digite um número válido:");
+            }
+
+            Console.WriteLine("Digite a operação (' / | + | * | - | %(MOD)'):");
+            operador = Console.ReadLine();
+
+            Console.WriteLine("Digite o segundo valor:");
+            valorEscolhido = Console.ReadLine();
+
+            isNumeroInteiro = double.TryParse(valorEscolhido, out num);
+            if (isNumeroInteiro)
+            {
+                num2 = Math.Round(double.Parse(valorEscolhido), 4);
+            }
+            else
+            {
+                Console.WriteLine("Digite um número válido:");
+            }
+
+            switch (operador)
+            {
+                case "+":
+                    prod = Math.Round(num1 + num2, 4);
+                    Console.WriteLine($"{ num1 } { '+' } { num2 } = {prod}");
+                    break;
+                case "-":
+                    prod = Math.Round(num1 - num2, 4);
+                    Console.WriteLine($"{ num1 } { '-' } { num2 } = {prod}");
+                    break;
+                case "*":
+                    prod = Math.Round(num1 * num2, 4);
+                    Console.WriteLine($"{ num1 } { '*' } { num2 } = {prod}");
+                    break;
+                case "/":
+                    prod = Math.Round(num1 / num2, 4);
+                    Console.WriteLine($"{num1} { '/' } {num2} = {prod}");
+                    break;
+                case "%":
+                    prod = Math.Round(num1 % num2, 4);
+                    Console.WriteLine($"{ num1 } { '%' } { num2 } = {prod}");
+                    break;
+                default:
+                    Console.WriteLine("Digite uma operação válida");
+                    break;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+    
+}
---
+++ Calculadora/ReadMe.txt
@@ -0,0 +1,11 @@
+****** pt - BR ******
+Este é meu primeiro projeto de programação em que trago para o GitHub.
+O mesmo foi feito em C# com a framework .NET Core utilizando a IDE Visual Studio Code edição Comunidade 
+da Microsoft.
+*********************
+
+****** en - US ******
+This is my first programming project that I bring to GitHub.
+The same was done in C# with the .NET Core framework using the IDE Visual Studio Code Community Edition
+from Microsoft.
+*********************
